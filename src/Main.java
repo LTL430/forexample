f85c6a2a21d5092e1e324bfc431c7b525bb3628d
LTL430 <lifengwu430@gmail.com>
2026-03-26 19:59:09 -0400

    temp

--- src/Main.java
+++ src/Main.java
@@ -3,19 +3,21 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		int sum=0; 
+		String sum=""; 
 		int n=0;
 		int m=0;
 		Scanner sc= new Scanner(System.in);
-		System.out.print("please input m:");
-		m=sc.nextInt();
+		//System.out.print("please input m:");
+		//m=sc.nextInt();
 		System.out.print("please input n:");
 		n=sc.nextInt();
-		for (int i=m;i<=n;i++){
-			sum = sum+i;
+		for (int i=1;i<=n;i++){
+			sum = sum+"*"; 
 		}
-		System.out.print(sum);
-
+		for(int i=1;i<=n;i++){
+			System.out.println(sum);
+		}
+	
 	}
 
 }
